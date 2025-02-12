db79cf9c1566d0c19e9e9a80ee8882449ff7042a
kaanguru <kaanguru@mail.ru>
2025-02-12 12:24:00 +0300

    gluestack-ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,23 @@
-import '../global.css';
+import '@/global.css';
 
 import { Stack } from 'expo-router';
+import { useState } from 'react';
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
+const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <GluestackUIProvider mode={colorMode}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
